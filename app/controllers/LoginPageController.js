65f77d4ae6ed962d8df693ca25a3578036ae3955
francescoriccardo <francescoriccardo1988@gmail.com>
2018-01-17 18:12:20 +0100

    navbar resize

--- app/controllers/LoginPageController.js
+++ app/controllers/LoginPageController.js
@@ -20,32 +20,31 @@ function LoginPageController($scope, RemoteCallService,StorageService,NavService
                     var butLogout=document.getElementById("butLogout");
 
                     var menu = document.getElementById("menu");
-                    menu.innerHTML="<a href=\"#!wheelsStore\" class=\"header item\">Wheels Store</a>\n" +
-                        "          <div class=\"ui simple dropdown item\">\n" +
-                        "              Scopri <i class=\"dropdown icon\"></i>\n" +
+                    menu.innerHTML="<a href=\"#!wheelsStore\" class=\"header item\">Wheels Store</a>" +
+                        "<a class=\"item\" href=\"#!menuAdmin\">Menu</a>    " +
+                        "    <div class=\"ui simple dropdown item\">" +
+                        "      Scopri <i class=\"dropdown icon\"></i>" +
+                        "              <div class=\"menu\">" +
+                        "                  <a class=\"item\" href=\"#!listaGomme\">I nostri pneumatici</a>" +
+                        "                  <a class=\"item\" href=\"#!newGomma\">Inserisci un nuovo pneumatico</a>" +
+                        "                  <a class=\"item\" href=\"#!search\">Ricerca pneumatico per brand</a>" +
+                        "                  <a class=\"item\" href=\"#!listaVehicle\">I nostri veicoli</a>" +
+                        "                  <a class=\"item\" href=\"#!insertVehicle\">Inserisci nuovo veicolo</a>" +
+                        "              </div>" +
+                        "          </div>" +
+                        "          <div class=\"ui simple dropdown item\">" +
+                        "              Chi siamo <i class=\"dropdown icon\"></i>" +
                         "              <div class=\"menu\">\n" +
-                        "                  <a class=\"item\" href=\"#!listaGomme\">I nostri pneumatici</a>\n" +
-                        "                  <a class=\"item\" href=\"#!newGomma\">Inserisci un nuovo pneumatico</a>\n" +
-                        "                  <a class=\"item\" href=\"#!search\">Ricerca pneumatico per brand</a>\n" +
-                        "                  <a class=\"item\" href=\"#!listaVehicle\">I nostri veicoli</a>\n" +
-                        "                  <a class=\"item\" href=\"#!insertVehicle\">Inserisci nuovo veicolo</a>\n" +
-                        "              </div>\n" +
-                        "          </div>\n" +
-                        "          <div class=\"ui simple dropdown item\">\n" +
-                        "              Chi siamo <i class=\"dropdown icon\"></i>\n" +
-                        "              <div class=\"menu\">\n" +
-                        "                  <a class=\"item\" href=\"#\">La nostra storia</a>\n" +
-                        "                  <div class=\"divider\"></div>\n" +
-                        "                  <a class=\"item\" href=\"#!about\">Contattaci</a>\n" +
-                        "              </div>\n" +
+                        "                  <a class=\"item\" href=\"#\">La nostra storia</a>"+
+                        "                  <a class=\"item\" href=\"#!about\">Contattaci</a>" +
+                        "              </div>" +
                         "          </div>";
 
 
                     var menuRight = document.getElementById("nav");
-                    menuRight.innerHTML = " <div class=\"item\">\n" +
-                        "        <i class=\"user icon\"></i>\n" +$scope.user.firstname+" "+$scope.user.lastname+
-                        "        \n" +
-                        "      </div>";
+                    menuRight.innerHTML = " <div class=\"item\">" +
+                        " <i class=\"user icon\"></i><p class=\"texticon\">" +$scope.user.firstname+" "+$scope.user.lastname+
+                        "" + "</p></div>";
                     menuRight.appendChild(butLogout);
                     NavService.set();
                     $location.path('/menuAdmin');
@@ -57,12 +56,16 @@ function LoginPageController($scope, RemoteCallService,StorageService,NavService
                         $scope.user = data.data.data;
                         StorageService.set("user",$scope.user);
 
+                        var menu = document.getElementById("menu");
+                        menu.innerHTML="<a href=\"#!wheelsStore\" class=\"header item\">Wheels Store</a>" +
+                            "<a class=\"item\" href=\"#!menuUser\">Menu<a/>";
+
                         var butLogout=document.getElementById("butLogout");
                         var menuRight = document.getElementById("nav");
-                        menuRight.innerHTML = " <div class=\"item\">\n" +
-                            "        <i class=\"user icon\"></i>\n" +$scope.user.firstname+" "+$scope.user.lastname+
-                            "        \n" +
-                            "      </div>";
+                        menuRight.innerHTML = " <div class=\"item\">" +
+                            "        <i class=\"user icon\"></i><p class=\"texticon\">" +$scope.user.firstname+" "+$scope.user.lastname+
+                            " " +
+                            " </p> </div>";
                         menuRight.appendChild(butLogout);
                         NavService.set();
 
